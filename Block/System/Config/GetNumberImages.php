2328d1a8b777be82e12a48329aba47a161a16110
Anton <anton@sussmanconsultants.com>
2019-06-24 00:55:25 +0700

    Actions: part 1

---
+++ Block/System/Config/GetNumberImages.php
@@ -0,0 +1,78 @@
+<?php
+namespace Ax\Zoom\Block\System\Config;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class GetNumberImages extends Field
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'Ax_Zoom::system/config/get_number_images.phtml';
+
+    /**
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Remove scope label
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    public function render(AbstractElement $element)
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+
+    /**
+     * Return element html
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        return $this->_toHtml();
+    }
+
+    /**
+     * Return ajax url for collect button
+     *
+     * @return string
+     */
+    public function getAjaxUrl()
+    {
+        return $this->getUrl('ax_zoom/system_config/collect');
+    }
+
+    /**
+     * Generate collect button html
+     *
+     * @return string
+     */
+    public function getButtonHtml()
+    {
+        $button = $this->getLayout()->createBlock(
+            'Magento\Backend\Block\Widget\Button'
+        )->setData(
+            [
+                'id' => 'get_number_images_button',
+                'label' => __('Get number images'),
+            ]
+        );
+
+        return $button->toHtml();
+    }
+}
+?>
