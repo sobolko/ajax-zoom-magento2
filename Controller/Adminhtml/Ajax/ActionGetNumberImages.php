651a4a08889158dc9f279da8963568d6fdb2b1f4
Anton <anton@sussmanconsultants.com>
2019-07-22 23:23:11 +0700

    Actions: Reset options/Get number Images

--- Controller/Adminhtml/Ajax/ActionGetNumberImages.php
+++ Controller/Adminhtml/Ajax/ActionGetNumberImages.php
@@ -1,31 +1,59 @@
 <?php
 namespace Ax\Zoom\Controller\Adminhtml\Ajax;
 
+//use Symfony\Component\Finder\Iterator\RecursiveDirectoryIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RegexIterator;
+
 class ActionGetNumberImages extends \Magento\Backend\App\Action
 {
 	protected $messageManager;
 	protected $_objectManager;
+	protected $Ax360set;
 
 	public function __construct(
 		\Magento\Backend\App\Action\Context $context,
-		\Magento\Framework\ObjectManagerInterface $objectManager
+		\Magento\Framework\ObjectManagerInterface $objectManager,
+		\Ax\Zoom\Model\Ax360set $Ax360set
 	)
 	{
 		$this->messageManager = $context->getMessageManager();
 		$this->_objectManager = $objectManager;
+		$this->Ax360set = $Ax360set;
 		parent::__construct($context);
 	}
 
 	public function execute()
 	{
         $get = $this->getRequest();
-        //$productId = $get->getParam('id_product');
         
+        $resource = $this->_objectManager->get('Magento\Framework\App\ResourceConnection');
+        $connection = $resource->getConnection();
+        $tableName = $resource->getTableName('catalog_product_entity_media_gallery'); //gives table name with prefix
+        $q = $connection->fetchAll('SELECT count(DISTINCT value) as numImg FROM `'.$tableName.'`');
+        $dst = $this->Ax360set->getBaseDir() . '/axzoom/pic/360/';
+        $img360 = count($this->rSearch($dst, '/\.(jpeg|jpg|png|gif|bmp|tif|tiff)/i'))/2;
 
         
-        
         die($this->_objectManager->create('Magento\Framework\Json\Helper\Data')->jsonEncode(array(
-            'confirmations' => array('The number images is ')
+        	'images2d' => $q[0]['numImg'],
+            'images360' => $img360
             )));
 	}
+
+    public function rSearch($folder, $pattern)
+    {
+        $dir = new RecursiveDirectoryIterator($folder);
+        $ite = new RecursiveIteratorIterator($dir);
+        $files = new RegexIterator($ite, $pattern, RegexIterator::GET_MATCH);
+        $fileList = array();
+
+        foreach ($files as $file) {
+            $fileList = array_merge($fileList, $file);
+        }
+
+        return $fileList;
+    }
+
 }
--- Controller/Adminhtml/Ajax/ActionReset.php
+++ Controller/Adminhtml/Ajax/ActionReset.php
@@ -5,27 +5,46 @@ class ActionReset extends \Magento\Backend\App\Action
 {
 	protected $messageManager;
 	protected $_objectManager;
+	protected $_cacheTypeList;
 
 	public function __construct(
 		\Magento\Backend\App\Action\Context $context,
-		\Magento\Framework\ObjectManagerInterface $objectManager
+		\Magento\Framework\ObjectManagerInterface $objectManager,
+		\Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
 	)
 	{
 		$this->messageManager = $context->getMessageManager();
 		$this->_objectManager = $objectManager;
+		$this->_cacheTypeList = $cacheTypeList;
 		parent::__construct($context);
 	}
 
 	public function execute()
 	{
         $get = $this->getRequest();
-        //$productId = $get->getParam('id_product');
-        
 
+		
+		/*
+	    $db = Mage::getSingleton('core/resource')->getConnection('core_write');
+        $db_prefix = (string)Mage::getConfig()->getTablePrefix();
+        $db->query('DELETE FROM `'.$db_prefix.'core_config_data` WHERE `path` LIKE \'axzoom_options/%\' AND path != \'axzoom_options/license/lic\'');
+
+        $this->sendJsonResponse(
+            array(
+                'status' => 'ok'
+            )
+        );
+        */
+
+        $resource = $this->_objectManager->get('Magento\Framework\App\ResourceConnection');
+        $connection = $resource->getConnection();
+        $tableName = $resource->getTableName('core_config_data'); //gives table name with prefix
+        $q = $connection->query('DELETE FROM `' . $tableName . '` WHERE `path` LIKE \'axzoom_options/%\' AND path != \'axzoom_options/license/lic\'');
+        $this->_cacheTypeList->cleanType('config');
         
         
         die($this->_objectManager->create('Magento\Framework\Json\Helper\Data')->jsonEncode(array(
-            'confirmations' => array('The data has been reset')
+            'status' => 'ok'
             )));
 	}
 }
